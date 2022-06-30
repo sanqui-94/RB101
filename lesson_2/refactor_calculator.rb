c95eeba138b8731982762b7057f08402024ca000
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2022-06-30 10:37:24 -0400

    refactor the calculator

---
+++ lesson_2/refactor_calculator.rb
@@ -0,0 +1,101 @@
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(operator)
+  result = case operator
+  when '1'
+    "adding"
+  when '2'
+    "substracting"
+  when '3'
+    "multiplying"
+  when '4'
+    "dividing"
+  end
+  result
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+name = ''
+loop {
+  name = Kernel.gets().chomp()
+  if name.empty?()
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+}
+
+prompt("Hi #{name}")
+loop do
+
+  number1 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  operation_prompt = <<-MSG
+      What operation would you like to perform
+      1) add
+      2) substract
+      3) multiply
+      4) divide
+  MSG
+
+  operator = ''
+  prompt(operation_prompt)
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Please use numbers from 1 to 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator #{name}. Good bye!")
