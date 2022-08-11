917577b2e6c41ee983966f53afa2c250a369d96b
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2022-08-10 23:03:59 -0400

    easy exercises part 1

---
+++ lesson_3/easy_1.rb
@@ -0,0 +1,11 @@
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+
+# `!` can be a negation operator, or it can be used to mark a function as
+# destructive that is, a method that will modify the caller, additionaly, the
+# exclamation sign can be used to convert a value into its boolean representation.
+# `?` can be a ternary operator to describe conditions, in other context
+# is used as part of the name of methods that will have a boolean return value.
